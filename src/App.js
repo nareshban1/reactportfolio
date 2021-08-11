5873fd7041063d434c18e59c5cc572f29d07547d
Naresh Ban <np03a170060@heraldcollege.edu.np>
2021-08-11 20:58:49 +0545

    added a loading screen

--- src/App.js
+++ src/App.js
@@ -9,29 +9,59 @@ import Projects from "./components/Projects/Projects";
 import RecentWork from "./components/RecentWork/RecentWork";
 import Designs from "./components/Designs/Designs";
 import Contact from "./components/Contact/Contact";
+import React, { useState, useEffect } from "react";
+import { css } from "@emotion/react";
+import RingLoader from "react-spinners/RingLoader";
+import DotLoader from "react-spinners/DotLoader";
 
-
+const override = css`
+  display: block;
+  margin: 0 auto;
+  border-color: #FD4370";
+`;
 
 function App() {
-    return(
-      <div classname="App">
-      <div style={{height:"30px" ,backgroundColor:"#FD4370", color:"white" ,fontFamily:"Poppins" , textAlign:"center"}}>
-        <p>This site is still under development.</p>
-      </div>
-      <Navbar/>
-      <div className="main_view">
-        <Home />
-        <RecentWork/>
-        <Skills/>
-        <About/>
-        <Timeline/>
-        <Projects/>
-        <Designs/>
-        <Contact/>
-      </div>
-      <Footer/>
-      </div>
-    )
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => setLoading(false), 6000);
+  }, []);
+
+  return (
+    <>
+      {!loading ? (
+        <div classname="App">
+          <div
+            style={{
+              height: "30px",
+              backgroundColor: "#FD4370",
+              color: "white",
+              fontFamily: "Poppins",
+              textAlign: "center",
+            }}
+          >
+            <p>This site is still under development.</p>
+          </div>
+          <Navbar />
+          <div className="main_view">
+            <Home />
+            <RecentWork />
+            <Skills />
+            <About />
+            <Timeline />
+            <Projects />
+            <Designs />
+            <Contact />
+          </div>
+          <Footer />
+        </div>
+      ) : (
+        <div className="loading">
+          <RingLoader color={"#FD4370"} css={override} size={150}></RingLoader>
+        </div>
+      )}{" "}
+    </>
+  );
 }
 
 export default App;
