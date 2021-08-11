43bac4a5893dd58e7205ed6dfe5ba04b9439c109
Naresh Ban <np03a170060@heraldcollege.edu.np>
2021-08-11 21:28:27 +0545

    done for today

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,6 +4,7 @@ import "./Footer.css"
 function Footer() {
     return (
         <footer className="footer">
+         <a href="http://nareshban.herokuapp.com/" target="_blank" rel="noreferrer" >Previous Portfolio</a>
         <p className="foottext">Created by Naresh Ban &copy; 2021 </p>
       </footer>
     )
