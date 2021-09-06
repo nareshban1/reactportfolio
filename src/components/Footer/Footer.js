705b679694a5b8e0f6cb0e33981eed8b6a47aba5
Naresh Ban <np03a170060@heraldcollege.edu.np>
2021-09-06 16:31:22 +0545

    added social  links

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,13 +1,37 @@
-import React from 'react'
-import "./Footer.css"
+import React from "react";
+import "./Footer.css";
 
+import {ImLinkedin,ImGithub} from "react-icons/im"
 function Footer() {
-    return (
-        <footer className="footer">
-         <a href="http://nareshban.herokuapp.com/" target="_blank" rel="noreferrer" >Previous Portfolio</a>
-        <p className="foottext">Created by Naresh Ban &copy; 2021 </p>
-      </footer>
-    )
+  return (
+    <footer className="footer">
+      <div className="socialicons">
+      <a
+        href="https://github.com/nareshban1"
+        target="_blank"
+        rel="noreferrer"
+      >
+          <ImGithub className="socialicon"/>
+          </a> <a
+        href="https://www.linkedin.com/in/naresh-ban-810b691a2/"
+        target="_blank"
+        rel="noreferrer"
+      >
+          <ImLinkedin className="socialicon"/>
+          </a>
+        </div>
+      
+      <a
+        href="http://nareshban.herokuapp.com/"
+        target="_blank"
+        rel="noreferrer"
+        className="olddesign"
+      >
+        Old Design Portfolio
+      </a>
+      <p className="foottext">Created by Naresh Ban &copy; 2021 </p>
+    </footer>
+  );
 }
 
-export default Footer
+export default Footer;
