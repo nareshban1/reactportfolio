02042cc7b48067bf83a00fef904263656dae3084
Naresh Ban <np03a170060@heraldcollege.edu.np>
2021-12-29 15:30:00 +0545

    google analytics

--- src/index.js
+++ src/index.js
@@ -2,7 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-
+import ReactGA from 'react-ga';
+ReactGA.initialize(`${process.env.REACT_APP_UA_ID}`); // add your tracking id here.
+ReactGA.pageview(window.location.pathname + window.location.search);
 
 
 
